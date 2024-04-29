62126f3dacbdfcf36e50ca8c1001a774ab42e878
Adam Leskis <leskis@gmail.com>
2024-04-29 22:04:55 +0100

    simplify components

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,32 +1,28 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
-
-interface FooterProps {
-  copyright: string;
-}
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import { useTheme } from '@mui/material/styles'
 
 const Footer = (): JSX.Element => {
-  const theme = useTheme();
+    const theme = useTheme()
 
-  return (
-    <Grid container spacing={2}>
-        <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
-          <Typography
-            align='center'
-            variant='subtitle2'
-            color={theme.palette.text.secondary}
-            gutterBottom
-            sx={{ marginTop: '25px' }}
-          >
-            Copyright &copy; {new Date().getFullYear()}.{' '}
-          </Typography>
-        </Box>
-    </Grid>
-  );
-};
+    return (
+        <Grid
+            container
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+            }}
+            spacing={4}
+        >
+            <Typography
+                variant="subtitle2"
+                color={theme.palette.text.secondary}
+            >
+                Copyright &copy; {new Date().getFullYear()}.{' '}
+            </Typography>
+        </Grid>
+    )
+}
 
-export default Footer;
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,32 @@
-import Services from '../components/Services';
-import Pricing from '../components/Pricing';
-import About from '../components/About';
-import Contact from '../components/Contact';
+import { useEffect } from 'react'
+import Services from '../components/Services'
+import Pricing from '../components/Pricing'
+import About from '../components/About'
+import Contact from '../components/Contact'
+// import Saas from '../components/Saas'
 
-    const Home = (): JSX.Element => {
-  return (
-    <div id='home'>
-      <Services />
-      <Pricing />
-      <About />
-      <Contact />
-    </div>
-  );
-};
+const Home = (): JSX.Element => {
+    // const [saasData, setSaasData] = useState([])
 
-export default Home;
+    useEffect(() => {
+        fetch('https://mytotallyserioussaasbusiness.com/api/saas')
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data.results)
+                // setSaasData(data.results)
+            })
+    }, [])
+
+    return (
+        <div id="home">
+            <Services />
+            <Pricing />
+            <About />
+            <Contact />
+        </div>
+    )
+}
+
+export default Home
