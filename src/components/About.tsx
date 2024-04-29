16bbf3b7a5f1bc06cc99543d15af6bb9a8027c1d
Adam Leskis <leskis@gmail.com>
2024-04-29 16:42:41 +0100

    delete more code we dont need

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,9 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import CountUp from 'react-countup';
-import VisibilitySensor from 'react-visibility-sensor';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 
-interface AboutProps {
-  value: number;
-  suffix: string;
-  description: string;
-}
-
 const About = (): JSX.Element => {
   const theme = useTheme();
-  const [viewPortEntered, setViewPortEntered] = useState(false);
-  const setViewPortVisibility = (
-    isVisible: boolean | ((prevState: boolean) => boolean)
-  ) => {
-    if (viewPortEntered) {
-      return;
-    }
-    setViewPortEntered(isVisible);
-  };
 
   return (
     <div id='about'>
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,35 +1,9 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useTheme } from '@mui/material/styles';
-
-interface PricingProps {
-  title: string;
-  price: number;
-  currency: string;
-  features: { name: string }[];
-}
 
 const Pricing = (): JSX.Element => {
   const theme = useTheme();
-
-  const handleButtonClick = () => {
-    const signupUrl = process.env.REACT_APP_SIGNUP_URL;
-
-    if (signupUrl) {
-      window.location.href = signupUrl;
-    } else {
-      console.error('Signup URL is undefined.');
-    }
-  };
 
   return (
     <div id='pricing'>
